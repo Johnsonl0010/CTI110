829fc3ed0c3b6b45914f7fa6bf6dc39f5f46c90b
Johnsonl0010 <31835418+Johnsonl0010@users.noreply.github.com>
2017-09-10 22:23:49 -0400

    Add files via upload

---
+++ M1T1_Johnson.py
@@ -0,0 +1,6 @@
+# M1T1
+# Hello World
+# Leon Johnson
+# 08/23/2017
+
+print ("Hello Wolrd")
---
+++ M2T1_SalesPrediction_Johnson.py
@@ -0,0 +1,22 @@
+# CTI-110
+# M2T1 - Sales Prediction
+# Leon Johnson
+# 09/10/2017
+
+
+# This program calculates 23 percent profits of Total Sales
+# Get input from user of projected sales
+
+totalSales =  int(input(' Please Enter the Projected Amount of Total Sales: '))
+
+#Percentage of total Sales to represetn annual profit                
+
+Percent =  (.23)
+
+# calculate projected profit
+# (int * percent -> result)
+
+annualProfit = totalSales * Percent
+
+# output results of projected profit
+print('The Projected Annual Profit is:', annualProfit)
